b7a0ff49472a555ceee979a440e667671b07a702
Seth Ellison <asylumtheinory@gmail.com>
2019-06-07 02:43:12 -0500

    Initial Commit - Basic Web App
    Will be filling this out with actual functionality tomorrow.

---
+++ src/main/java/metrics/MetricsAppApplication.java
@@ -0,0 +1,12 @@
+package metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsAppApplication.class, args);
+	}
+}
